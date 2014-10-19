46e8dc8be3e2217fdd949d85d850c87627c3dfbe
Administrator <filippo.fratoni@gmail.com>
2014-10-19 18:49:38 +0200

    Aroon kikck off

---
+++ src/main/groovy/ta/Aroon.java
@@ -0,0 +1,28 @@
+package ta;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * groovy-playground
+ * Created by filippo on 19/10/14.
+ */
+public class Aroon {
+    private static Logger log= LoggerFactory.getLogger(Aroon.class);
+
+
+    public static double[][] aroon(){
+        return null;
+    }
+
+    public static double[] aroonUp(){
+        return null;
+    }
+    public static double[] aroonDown(){
+        return null;
+    }
+
+    public static double[] aroonOscillator(){
+        return null;
+    }
+}
