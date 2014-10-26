4796a24de3f5c0862daa0efffd2208f8b76b7d9a
Administrator <filippo.fratoni@gmail.com>
2014-10-26 20:36:44 +0100

    aroon ascillator

--- src/main/groovy/ta/Aroon.java
+++ src/main/groovy/ta/Aroon.java
@@ -4,6 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
+ * The Aroon indicators fluctuate above/below a centerline (50) and are bound between 0 and 100.
+ * These three levels are important for interpretation. At its most basic, the bulls have the edge when Aroon-Up is
+ * above 50 and Aroon-Down is below 50. This indicates a greater propensity for new x-day highs than lows. The
+ * converse is true for a downtrend. The bears have the edge when Aroon-Up is below 50 and Aroon-Down is above 50.
  * groovy-playground
  * Created by filippo on 19/10/14.
  */
@@ -14,7 +18,7 @@ public class Aroon {
      * combined Aroon indicators
      * @return AroonUp,AroonDown and AroonOscillator in this order
      */
-    public static double[][] aroon(){
+    public static double[] aroonSignal(){
         return null;
     }
 
@@ -39,8 +43,14 @@ public class Aroon {
         return aroonDown;
     }
 
-    public static double[] aroonOscillator(){
-        return null;
+    public static double[] aroonOscillator(double[] highs,double[] lows,int periods){
+        double[] aroonUp=aroonUp(highs,periods);
+        double[] aroonDown=aroonDown(lows, periods);
+        double[] aroonOscillator= new double[highs.length];
+        for (int i = 0; i < aroonUp.length; i++) {
+            aroonOscillator[i]=aroonUp[i]-aroonDown[i];
+        }
+        return aroonOscillator;
     }
 
     /**
--- src/test/groovy/ta/AroonTest.groovy
+++ src/test/groovy/ta/AroonTest.groovy
@@ -26,4 +26,19 @@ class AroonTest {
         ArrayHelper.log(aroonDown,log,true)
 
     }
+
+    @Test
+    def void aroonOscillator(){
+        def highs=[1,2,3,4,5,6,7,6,6,5,4,3,13,14,15,16,17,16,15,11,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37] as double[]
+        double[] aroonUp=Aroon.aroonUp(highs,25)
+
+        def lows=[1,1,2,3,3,3,3,4,5,6,6,7,8,9,10,9,9,10,9,13,14,15,16,17,18,19,20,21,21,22,23,24,25,25,26,27,28] as double[]
+        double[] aroonDown=Aroon.aroonUp(lows,25)
+
+        double[] aroonOscillator=Aroon.aroonOscillator(highs,lows,25)
+
+        ArrayHelper.log(aroonOscillator,log,true)
+
+
+    }
 }
